efa771f3775a387686c5d7ca9854c78430bdb646
Juan Gomes Macario <jgomesmacario@outlook.com>
2024-03-16 01:59:15 -0300

    first commit

---
+++ src/components/Ocarina3D.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Spline from "@splinetool/react-spline";
+
+const Ocarina3D = () => {
+    return (
+        <Spline scene="https://prod.spline.design/btim9K6KaIE0CJzO/scene.splinecode" />
+    )
+}
+
+export default Ocarina3D;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        nihon: {
+          red: "#EA1646",
+          blue: "#0000FF",
+          green: "#00FF00",
+          yellow: "#FFFF00",
+          orange: "#FFA500",
+          purple: "#800080",
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
