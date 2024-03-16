c0d351b6e6c6615eeca49de9f3c94a70a67df682
Juan Gomes Macario <jgomesmacario@outlook.com>
2024-03-16 02:23:06 -0300

    arrumar responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header>
-        <div className="container mx-auto">
+      <header className="">
+        <div className="container px-4 mx-auto">
           <div className="flex items-center justify-between py-16">
             <h1 className="text-5xl text-nihon-red">龍</h1>
-            <nav className="flex items-center space-x-6 z-10">
+            <nav className="items-center space-x-6 z-10 hidden md:flex">
               <a href="/home" className="border-b border-b-nihon-red">ホーム</a>
               <a href="/about">私について</a>
               <a href="/portfolio">ポートフォリオ</a>
@@ -18,13 +18,13 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <div className="flex-1 container mx-auto flex justify-end items-stretch relative">
-        <div className="fixed inset-0 -left-1/3">
+      <div className="flex-1 container px-4 mx-auto flex justify-end items-stretch relative">
+        <div className="fixed inset-0 md:-left-1/3">
           <Ocarina3D />
         </div>
-        <span className="[writing-mode:vertical-rl] text-5xl text-center">技術によって、夢<br />は現実に変わる。</span>
+        <span className="[writing-mode:vertical-rl] md:text-5xl text-center">技術によって、夢<br className="hidden md:block" />は現実に変わる。</span>
       </div>
-      <div className="container mx-auto">
+      <div className="container px-4 mx-auto">
         <div className="flex flex-col py-16">
           <h2 className="text-white text-2xl z-10">クラッシュユニックス</h2>
           <span className="text-nihon-red z-10">ソフトウェア開発者</span>
