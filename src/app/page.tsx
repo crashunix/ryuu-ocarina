235c4bea6dd2328eaa0ad634ac6bb9ab6f7a220e
Juan Gomes Macario <jgomesmacario@outlook.com>
2024-03-16 02:16:23 -0300

    ajustar z-index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="container mx-auto">
           <div className="flex items-center justify-between py-16">
             <h1 className="text-5xl text-nihon-red">龍</h1>
-            <nav className="flex items-center space-x-6">
+            <nav className="flex items-center space-x-6 z-10">
               <a href="/home" className="border-b border-b-nihon-red">ホーム</a>
               <a href="/about">私について</a>
               <a href="/portfolio">ポートフォリオ</a>
@@ -19,15 +19,15 @@ export default function Home() {
         </div>
       </header>
       <div className="flex-1 container mx-auto flex justify-end items-stretch relative">
-        <div className="fixed inset-0 -top-[10%] -left-1/3">
+        <div className="fixed inset-0 -left-1/3">
           <Ocarina3D />
         </div>
         <span className="[writing-mode:vertical-rl] text-5xl text-center">技術によって、夢<br />は現実に変わる。</span>
       </div>
       <div className="container mx-auto">
         <div className="flex flex-col py-16">
-          <h2 className="text-white text-2xl">クラッシュユニックス</h2>
-          <span className="text-nihon-red">ソフトウェア開発者</span>
+          <h2 className="text-white text-2xl z-10">クラッシュユニックス</h2>
+          <span className="text-nihon-red z-10">ソフトウェア開発者</span>
         </div>
       </div>
     </div>
